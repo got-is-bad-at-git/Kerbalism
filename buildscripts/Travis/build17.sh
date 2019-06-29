83df97d9da779ca2df48bbedc3958f4a8b3b0aa2
SirMortimer <his.mortimerness@gmail.com>
2019-06-29 09:02:03 +0200

    don't re-download the same file again

--- buildscripts/Travis/build17.sh
+++ buildscripts/Travis/build17.sh
@@ -7,13 +7,14 @@ current_kspvr="1.7.1"
 current_kspbin="17"
 echo "Building for $current_kspvr / $current_kspbin"
 filename="KSP-$current_kspvr.7z"
-wget "https://img.steamport.xyz/$filename"
+if [ ! -f $filename ]; then
+	wget "https://img.steamport.xyz/$filename"
+fi
 mkdir "src/DLLs"
 7za x $filename -osrc/DLLs -pgQn337XZBEFxzFuVwzKgc27ehZo7XLz485hh3erqF9
 bash "buildscripts/Travis/avc_to_assembly.sh"
 msbuild /p:DefineConstants="KSP${current_kspbin}" Kerbalism.sln /t:Build /p:Configuration="Release"
 /bin/cp -rf "src/KerbalismBootstrap/obj/Release/KerbalismBootstrap.dll" "GameData/Kerbalism/KerbalismBootstrap.dll"
 /bin/cp -rf "src/Kerbalism/obj/Release/Kerbalism.dll" "GameData/Kerbalism/Kerbalism${current_kspbin}.kbin"
-rm -rf $filename
 
 rm -f GameData/Kerbalism/Kerbalism.dll
